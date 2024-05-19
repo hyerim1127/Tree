c0e9d9f393ea64e706ef1cd866a499d16b76927d
hyerim <hyerim1148@naver.com>
2024-05-20 00:21:43 +0900

    9 - 모달 스크롤바 수정

--- src/impression/ModalImpression.jsx
+++ src/impression/ModalImpression.jsx
@@ -4,13 +4,13 @@ import ToggleDetail from "./ToggleDetail";
 
 const ModalImpression = ({ book, onClose }) => {
 
-    return (
-      <div className="I-modal">
-        <div className="I-modal-content">
-          <span className="I-modal-close" onClick={onClose}>&times;</span>
+  return (
+    <div className="I-modal">
+      <div className="I-modal-content">
+        <span className="I-modal-close" onClick={onClose}>&times;</span>
 
-          <div className="I-modal-body">
-            <div style={{display: 'flex', alignItems: 'stretch',}}>
+        <div className="I-modal-body">
+          <div style={{display: 'flex', alignItems: 'stretch',}}>
             <div className="I-book-image">
               <BookImage src={book.image} />
             </div>
@@ -20,19 +20,18 @@ const ModalImpression = ({ book, onClose }) => {
                 author={book.author} 
                 description={book.description} 
               />
-            </div>
-            </div>
-            <div className="toggle-sections">
-              {book.details.map((detail, index) => (
-                <ToggleDetail key={index} detail={detail} />
-              ))}
-            </div>
           </div>
-
         </div>
-      </div>
-  
-    );
-  };
+        <div className="toggle-sections">
+          {book.details.map((detail, index) => (
+            <ToggleDetail key={index} detail={detail} />
+          ))}
+          </div>
+        </div>
 
-  export default ModalImpression;
+      </div>
+    </div>
+  );
+};
+
+export default ModalImpression;
