03c5dcb455cdd9d08d085ae82ff56e78137b7f61
hyerim <hyerim1148@naver.com>
2024-06-06 21:24:16 +0900

    -

--- src/main/frontend/src/Login.jsx
+++ src/main/frontend/src/Login.jsx
@@ -64,7 +64,7 @@ export default function Login() {
             console.log('email:', email);
             console.log('password:', pw);
             setIsLoggedIn(true);
-            axios.post('http://localhost:8081/member/login', {
+            axios.post('http://localhost:8081/', {
 
                     memberEmail: email,
                     memberPassword: pw
